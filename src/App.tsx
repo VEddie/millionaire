3c30612b4d3d25bfe8a65239dc5b1475579e3c97
VEddie <edijose18@gmail.com>
2025-03-24 19:00:03 -0400

    Initial commit, Chakra UI added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Text } from '@chakra-ui/react';
+import { useState } from 'react';
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <Text>Hello</Text>
+        </>
+    );
+}
+
+export default App;
